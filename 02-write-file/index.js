79211760d3a97a8a5ad72a651c935de2f973b506
dasuchka <nastya_d1199@mail.ru>
2022-05-24 12:47:00 +0300

    feat: made task 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,10 +1,44 @@
 const fs = require("fs");
 const path = require("path");
+const textFile = path.join(__dirname, "text.txt");
 
-const file = path.join(__dirname, "text.txt");
+const readline = require('readline');
+const intf =readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
 
-fs.writeFile(file, "i'm here", err => {
+fs.writeFile(textFile, '', err => {
     if (err) {
         throw err
     }
 })
+
+intf.question('Please, enter something:\n', (input) =>{
+    if (input === "") return console.log ("You write nothing! Please, write again");
+    fs.appendFile(textFile, input+'\n', err => {
+        if (err) {
+            throw err
+        }
+    })
+})
+
+intf.on('line', (input) => {
+    if (input === "exit"){
+        intf.on('close', () => {
+            console.log('Thank you, goodbye!')
+        });
+        return intf.close();
+    }
+    fs.appendFile(textFile, input+'\n', err => {
+        if (err) {
+            throw err
+        }
+    })
+})
+
+intf.on('SIGINT', () => {
+    console.log('Thank you, goodbye!');
+    return intf.close();
+})
+
